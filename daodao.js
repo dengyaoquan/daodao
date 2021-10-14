66696494c4fe9e41bbd28cd3c503423e24518e40
dengyaoquan <75103938+dengyaoquan@users.noreply.github.com>
2021-10-14 15:11:12 +0800

    Update daodao.js

--- daodao.js
+++ daodao.js
@@ -1,3 +1,5 @@
+<meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+
 function getLocalTime(nS) {
     return new Date(parseInt(nS) * 1000).toLocaleString().replace(/:\d{1,2}$/,' ');
 }
